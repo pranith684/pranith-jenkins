28515fba4ae5852ae2f5dc8124e317ec8710d2ab
pranith684 <solasapranith@gmail.com>
2026-02-16 15:52:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         {
+            int a=20;
+            int b=10;
+            System.out.println("Total is:"+(a+b));
             System.out.println("Hello Jenkins from Java App");
         }
     }
